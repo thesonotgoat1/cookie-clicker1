d0e1e0d79d354a7cca73571890e7d306c6bac3c4
thesonotgoat1 <155997029+thesonotgoat1@users.noreply.github.com>
2025-11-14 09:14:47 -0600

    update 1.1

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 let cookies = 10000;
 let cursors = 0;
 let grandmas = 0;
+let farms = 0;
 let cps = 0;
+let timer = 0;
 
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
@@ -12,6 +14,30 @@ const cursorCount = document.getElementById("cursorCount");
 const grandmaButton = document.getElementById("grandmaButton");
 const buyGrandma = document.getElementById("grandmaCost");
 const grandmaCount = document.getElementById("grandmaCount");
+const farmButton = document.getElementById("farmButton");
+const buyFarm = document.getElementById("farmCost");
+const farmCount = document.getElementById("farmCount");
+
+cookie.onmousedown = () => cookie.style.scale = "0.9";
+cookie.onmouseup = () => cookie.style.scale = "1.1";
+cookie.onmouseenter = () => {
+  cookie.style.scale = "1.1";
+};
+
+cookie.onmouseleave = () => {
+  cookie.style.scale = "1";
+};
+
+function loop() {
+  timer += 0.0004; // smaller = smoother + slower
+
+  // rotation formula
+  const angle = 0 + (Math.sin(timer * 90) * 10);
+
+  cookie.style.transform = `rotate(${angle}deg)`;
+
+  requestAnimationFrame(loop);
+}
 
 function cursorCost() {
   return Math.floor(15 * 1.15 ** cursors);
@@ -21,18 +47,25 @@ function grandmaCost() {
   return Math.floor(100 * 1.15 ** grandmas);
 }
 
-function updateUI() {
+function farmCost() {
+  return Math.floor(1100 * 1.15 ** farms);
+}
+
+function updatestuf() {
+  cps = cursors * 0.1 + grandmas * 1;
   counter.textContent = `Cookies: ${Math.floor(cookies)}`;
   cpsDisplay.textContent = `Cookies per second: ${cps.toFixed(1)}`;
   buyCursor.textContent = `Cursor (${cursorCost()} cookies)`;
   cursorCount.textContent = cursors;
   buyGrandma.textContent = `Grandma (${grandmaCost()} cookies)`;
   grandmaCount.textContent = grandmas;
+  buyFarm.textContent = `farm (${farmCost()} cookies)`;
+  farmCount.textContent = farms;
 }
 
 cookie.onclick = () => {
   cookies++;
-  updateUI();
+  updatestuf();
 };
 
 cursorButton.onclick = () => {
@@ -40,9 +73,8 @@ cursorButton.onclick = () => {
   if (cookies >= cost) {
     cookies -= cost;
     cursors++;
-    updateCPS();
-    updateUI();
-  } else { }
+    updatestuf;
+  } else {}
 };
 
 grandmaButton.onclick = () => {
@@ -50,19 +82,23 @@ grandmaButton.onclick = () => {
   if (cookies >= cost) {
     cookies -= cost;
     grandmas++;
-    updateCPS();
-    updateUI();
-  } else { }
+    updatestuf();
+  } else {}
 };
 
-function updateCPS() {
-  cps = cursors * 0.1 + grandmas * 1;
-}
+farmButton.onclick = () => {
+  const cost = farmCost();
+  if (cookies >= cost) {
+    cookies -= cost;
+    farms++;
+    updatestuf();
+  } else {}
+};
 
 setInterval(() => {
   cookies += cps / 10;
   updateUI();
 }, 100);
 
-updateCPS();
-updateUI();
+updatestuf();
+loop();
